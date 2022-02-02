b73941651c094d6928bb8d355335986ed58faf9f
rbernar2 <66698917+rbernar2@users.noreply.github.com>
2022-02-01 21:33:30 -0500

    Add project files.

---
+++ CSharpPracticeConcerts/Concert.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CSharpPracticeConcerts
+{
+    public class Concert
+    {
+        private string headlinerName;
+        private string concertDate;
+        private string concertVenue;
+        private float ticketPrice;
+
+        List<string> openerNames = new List<string>();
+
+        public Concert(string headlinerName, string concertDate, string concertVenue, float ticketPrice)
+        {
+            this.headlinerName = headlinerName;
+            this.concertDate = concertDate;
+            this.concertVenue = concertVenue;
+            this.ticketPrice = ticketPrice;
+        }//end ctor
+
+        public void addOpener(string openerName)
+        {
+            openerNames.Add(openerName);
+
+        }//end addOpener
+
+        public void printInfo()
+        {
+            Console.WriteLine(
+                "=====THE CROFOOT PRESENTS=====\n\n" + headlinerName +
+                " \n\n" + "With Supporting Acts: \n\n");
+
+            foreach (var openerNames in openerNames)
+            {
+                Console.WriteLine(openerNames);
+            }
+
+            Console.WriteLine("\n\n" + "Show Details:" + "\n\n" + concertDate + ", " +
+            concertVenue + " at The Crofoot Complex.  " + "$" + ticketPrice);
+
+            Console.ReadLine();
+        }//end print info
+
+    }//end public class
+
+}
---
+++ CSharpPracticeConcerts/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpPracticeConcerts
+{
+    public class MainClass
+    {
+        static void Main(String[] args)
+        {
+            //This class uses the Concert class that you are to create. It takes in the name headliner, date, stage, and ticket price.
+            //You will be able to add as many openers as needed.
+            //Calling the printInfo method for any given concert should output text that matches what I sent. Have fun!
+
+            // ONE OK ROCK CONCERT
+            Concert oneOkRock = new Concert("ONE OK ROCK", "10/1/2020", "Crofoot Ballroom", 50f); //making a new concert object takes in name of headliner, date of show, stage name and ticket prices
+
+            oneOkRock.addOpener("Simple Plan"); //Adding openers, easiest way is to probably use an ArrayList of Strings
+            oneOkRock.addOpener("5 Seconds of Summer");
+
+            oneOkRock.printInfo(); //Prints all info to the console
+
+
+            //YUNGBLUD CONCERT
+            Concert yungblud = new Concert("Yungblud", "11/15/2020", "Pike Room", 20f);
+
+            yungblud.addOpener("Saint Phnx");
+            yungblud.addOpener("Halsey but she's in disguise");
+
+            yungblud.printInfo();
+
+        }
+    }
+}
+
